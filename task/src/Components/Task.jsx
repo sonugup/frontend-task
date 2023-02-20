ff1e9f139a0edded845e38edb727c9f647d54993
Sonu Gupta <rg3029926@gmail.com>
2023-02-21 00:30:50 +0530

    complete task

--- task/src/Components/Task.jsx
+++ task/src/Components/Task.jsx
@@ -1,53 +1,84 @@
 import React, {useEffect} from 'react';
 import {shallowEqual, useDispatch, useSelector} from "react-redux"
 import TaskInput from './TaskInput'
-import axios from "axios"
-import {getTaskRequest, getTaskSuccess, GetTaskFailure} from "../Redux/action"
+import axios from "axios";
+import "./task.css"
+import {getTaskRequest, getTaskSuccess, getTaskFailure, postTaskRequest, postTaskSuccess, postTaskFailure} from "../Redux/action"
 
 const Task = () => {
     const dispatch=useDispatch()
-    const isLoading=useSelector((state) => state.isLoading);
+    // const isLoading=useSelector((state) => state.isLoading);
 
-    const task=useSelector(state => state.task);
+    // const task=useSelector(state => state.task);
 
-    // const {task, isLoading, isError} = useSelector((state) => {
-    //     return {
-    //         task: state.Task,
-    //         isLoading:state.isLoading,
-    //         isError:state.isError,
-    //     }
-    // }, shallowEqual)
+    const {task, isLoading, isError} = useSelector((state) => {
+        return {
+            task: state.task,
+            isLoading:state.isLoading,
+            isError:state.isError,
+        }
+    }, shallowEqual)
     const getTask = () => {
         const getTaskReqActObj=getTaskRequest();
         dispatch(getTaskReqActObj);
-        axios.get("http://localhost:8080/task")
+        return axios.get("http://localhost:8080/task")
         .then((r) => {
             dispatch(getTaskSuccess(r.data));
         })
         .catch((e) => {
-            dispatch(getTaskRequest())
+            dispatch(getTaskFailure())
             console.log(e)
         })
     };
+
+    const addTask = (title) => {
+        if(title) {
+           const payload= {
+            title,
+           }
+
+           dispatch(postTaskRequest());
+           return  axios.post("http://localhost:8080/task", payload).then(r => {
+            dispatch(postTaskSuccess(r.data))
+           }).catch(e=> {
+            dispatch(postTaskFailure())
+            console.log(e)
+           })
+        }
+
+    }
+
+    const handleAddTask = (text) => {
+        addTask(text).then(() => getTask()).then(() => {
+            console.log("data add successfully")
+        });
+    }
+
+    
 
     useEffect(() => {
     getTask()
     }, [])
   return (
-    <div>
-      <h1>Task</h1>
+    <div className='task'>
+      <h1 className='nav'>React js Frontend Task</h1>
+      <div className='cont'>
       {
-        isLoading ? <div>Loading...</div>: <TaskInput/>
+        isLoading ? <div>Loading...</div>: <TaskInput handleAddTask={handleAddTask}/>
       }
       {
-        task.length> 0 && task.map((item) => {
+        task.length> 0 && task.map((item, id) => {
             return (
-                <div>
-                    <h3> {item.title} </h3>
+                <div  className="box">
+                    <tr key={id}>
+                    <td> {item.title} </td>
+                </tr>
                 </div>
+                
             )
         })
       }
+      </div>
       
     </div>
   )
--- task/src/Components/TaskInput.jsx
+++ task/src/Components/TaskInput.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const TaskInput = () => {
+const TaskInput = ({handleAddTask}) => {
+  const [text, setText] = useState("")
+
+  const handlClick = () => {
+    if(text){
+      handleAddTask(text);
+      setText("")
+    }
+  }
   return (
     <div>
-      <h1>TaskInput</h1>
-      <input type="text" name="task" id="" placeholder='Add Task '/>
+      <input type="text" name="task" onChange={(e) => setText(e.target.value)} id="inp" placeholder='Enter the Task '/>
+      <button className='btn' onClick={handlClick}>Add</button>
     </div>
   )
 }
