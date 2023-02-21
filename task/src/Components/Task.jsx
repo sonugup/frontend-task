bba53ec95cb1e0606d37c519ba7fd456d31ce3fe
Sonu Gupta <rg3029926@gmail.com>
2023-02-21 23:39:46 +0530

    add file

--- task/src/Components/Task.jsx
+++ task/src/Components/Task.jsx
@@ -1,49 +1,49 @@
-import React, {useEffect} from 'react';
-import {shallowEqual, useDispatch, useSelector} from "react-redux"
+import React, { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from "react-redux"
 import TaskInput from './TaskInput'
 import axios from "axios";
 import "./task.css"
-import {getTaskRequest, getTaskSuccess, getTaskFailure, postTaskRequest, postTaskSuccess, postTaskFailure} from "../Redux/action"
+import { getTaskRequest, getTaskSuccess, getTaskFailure, postTaskRequest, postTaskSuccess, postTaskFailure } from "../Redux/action"
 
 const Task = () => {
-    const dispatch=useDispatch()
+    const dispatch = useDispatch()
     // const isLoading=useSelector((state) => state.isLoading);
 
     // const task=useSelector(state => state.task);
 
-    const {task, isLoading, isError} = useSelector((state) => {
+    const { task, isLoading, isError } = useSelector((state) => {
         return {
             task: state.task,
-            isLoading:state.isLoading,
-            isError:state.isError,
+            isLoading: state.isLoading,
+            isError: state.isError,
         }
     }, shallowEqual)
     const getTask = () => {
-        const getTaskReqActObj=getTaskRequest();
+        const getTaskReqActObj = getTaskRequest();
         dispatch(getTaskReqActObj);
         return axios.get("http://localhost:8080/task")
-        .then((r) => {
-            dispatch(getTaskSuccess(r.data));
-        })
-        .catch((e) => {
-            dispatch(getTaskFailure())
-            console.log(e)
-        })
+            .then((r) => {
+                dispatch(getTaskSuccess(r.data));
+            })
+            .catch((e) => {
+                dispatch(getTaskFailure())
+                console.log(e)
+            })
     };
 
     const addTask = (title) => {
-        if(title) {
-           const payload= {
-            title,
-           }
+        if (title) {
+            const payload = {
+                title,
+            }
 
-           dispatch(postTaskRequest());
-           return  axios.post("http://localhost:8080/task", payload).then(r => {
-            dispatch(postTaskSuccess(r.data))
-           }).catch(e=> {
-            dispatch(postTaskFailure())
-            console.log(e)
-           })
+            dispatch(postTaskRequest());
+            return axios.post("http://localhost:8080/task", payload).then(r => {
+                dispatch(postTaskSuccess(r.data))
+            }).catch(e => {
+                dispatch(postTaskFailure())
+                console.log(e)
+            })
         }
 
     }
@@ -54,34 +54,33 @@ const Task = () => {
         });
     }
 
-    
+
 
     useEffect(() => {
-    getTask()
+        getTask()
     }, [])
-  return (
-    <div className='task'>
-      <h1 className='nav'>React js Frontend Task</h1>
-      <div className='cont'>
-      {
-        isLoading ? <div>Loading...</div>: <TaskInput handleAddTask={handleAddTask}/>
-      }
-      {
-        task.length> 0 && task.map((item, id) => {
-            return (
-                <div  className="box">
-                    <tr key={id}>
-                    <td> {item.title} </td>
-                </tr>
-                </div>
-                
-            )
-        })
-      }
-      </div>
-      
-    </div>
-  )
+    return (
+        <div className='task'>
+            <h1 className='nav'>React js Frontend Task</h1>
+            <div className='cont'>
+                {
+                    isLoading ? <div>Loading...</div> : <TaskInput handleAddTask={handleAddTask} />
+                }
+                {
+                    task.length > 0 && task.map((item, id) => {
+                        return (
+                            <div className="box">
+                                <tr key={id}>
+                                    <td> {item.title} </td>
+                                </tr>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    )
 }
 
 export default Task
