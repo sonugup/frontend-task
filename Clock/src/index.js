0dcd3394cfd1d9c40df7e4f4d80f6e12b81059cf
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-03-30 13:14:45 +0530

    Update index.js

--- Clock/src/index.js
+++ Clock/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
