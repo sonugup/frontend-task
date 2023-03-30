7d5fcb524502aaefe3ec817e88dff9cf585a66bd
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-03-30 13:13:16 +0530

    Update index.js

--- Clock/src/index.js
+++ Clock/src/index.js
@@ -13,6 +13,6 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+
 
 reportWebVitals();
